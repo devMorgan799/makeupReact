8a0e6818972a5e53a5054421612b29b834eabfd1
Student_113 <student_113@Student-MA113.home>
2020-04-26 18:55:58 -0400

    submit button

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,21 @@ import React, { Component } from 'react';
 class Home extends Component {
    
     render() {
-        console.log(this.props.makeupDetails[0]);
+        // console.log(this.props.makeupDetails[0]);
         return (
             <div>
                {/* <h1>{this.prop.brands.brand}</h1>  */}
+               <h1>Look up eco-friendly makeup from the brands below</h1>
+               <div className="dropdownMenu">
+                   <select className="brandsMenu" onChange={this.onChange}>
+                   <option>Makeup Brands</option>
+                   <option>l'oreal</option>
+                   <option>Maybelline</option>
+                   <option>Smashbox</option>
+                   <option>Wet n Wild</option>
+
+                   </select>
+                </div>
             </div>
         );
     }
--- src/components/MakeupBrands.js
+++ src/components/MakeupBrands.js
@@ -1,10 +1,55 @@
 import React, { Component } from 'react';
+import axios from "axios";
+
 
 class MakeupBrands extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+    
+          data: [],
+          brand: [],
+    
+        }
+      }
+
+  componentDidMount() {
+    // axios.get(`http://makeup-api.herokuapp.com/api/v1/products.json?brand=${this.state.brand}&tag_list=organic`)
+     axios.get("http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline&tag_list=organic")
+     .then(res => {
+      const makeupInfo = res.data;
+      // console.log("makeupInfo ---->", makeupInfo)
+      
+      this.setState({data: makeupInfo})
+      // console.log(this.state.data)
+      //this.state.data is the same info as makeupInfo. the data is 
+      //just placed in diff. locattion. the state places info into app
+
+    })
+    .catch(error => {
+      console.log('there is an eror', error)
+    })
+
+  }
+  onChange = (event) => (this.setState({[event.target.name]: event.target.value })); 
+  onSubmit = (event) => {
+      event.preventDefault();
+      this.setState({[event.target.name]:event.target.value})
+      this.convert()
+  }
     render() {
         return (
-            <div>
-                
+            <div className="dropdownMenu" onSubmit= {this.onSubmit}>
+                <form className= "form">
+                    <select className="brandsMenu" name= "brand" onChange={this.onChange}>
+                        <option>Makeup Brands</option>
+                        <option value= "l'oreal">l'oreal</option>
+                        <option value="Maybelline">Maybelline</option>
+                        <option value= "Smashbox">Smashbox</option>
+                        <option value= "Wetnwild">Wet n Wild</option>
+                    </select>
+                </form>
             </div>
         );
     }
