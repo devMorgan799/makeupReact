717f8668ee0c611970e4f2aef8821327f9ea6c33
Student_113 <student_113@Student-MA113.home>
2020-04-26 17:37:37 -0400

    aboutpage

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 class About extends Component {
     render() {
         return (
-            <div>
+            <div className= "aboutPic">
+               <h1>Eco-friendly Makeup</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam cum illum soluta reprehenderit earum totam reiciendis natus non officiis doloremque ex, quae iste, eos doloribus</p>
+{/* 
+                <img src="https://ksr-ugc.imgix.net/assets/011/655/381/c66472759388df1ae450be34be488a75_original.jpg?ixlib=rb-2.1.0&crop=faces&w=1552&h=873&fit=crop&v=1463686329&auto=format&frame=1&q=92&s=ba0c15579b73ac3f964d3ff815fb408a" alt= "brushes"/> */}
+
+        
+
                 
             </div>
         );
---
+++ src/components/MakeupBrands.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MakeupBrands extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MakeupBrands;
