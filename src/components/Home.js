81ab9b91df077a0ba0ee1563e92d4ba68eb933df
Student_113 <student_113@Student-MA113.home>
2020-04-26 19:22:17 -0400

    dropdown

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,16 +9,7 @@ class Home extends Component {
             <div>
                {/* <h1>{this.prop.brands.brand}</h1>  */}
                <h1>Look up eco-friendly makeup from the brands below</h1>
-               <div className="dropdownMenu">
-                   <select className="brandsMenu" onChange={this.onChange}>
-                   <option>Makeup Brands</option>
-                   <option>l'oreal</option>
-                   <option>Maybelline</option>
-                   <option>Smashbox</option>
-                   <option>Wet n Wild</option>
-
-                   </select>
-                </div>
+              
             </div>
         );
     }
